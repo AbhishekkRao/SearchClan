3593d590d8cb3a0b18d3b32fb0a13af326224784
Vasu Pal <vasupal.17.12.2002@gmail.com>
2023-12-23 13:21:29 +0530

    second commit

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -7,6 +7,11 @@ const Home = () => {
 		<div className='w-full h-full bg-light-yellow overflow-auto'>
 			<div className='h-1/10 m-8 flex justify-between'>
 				<img src={logo} alt='LOGO' className='w-32' />
+				<div className='w-1/3 h-12 mt-8 bg-white rounded-3xl shadow-lg'>
+					<div className='px-12 py-3 uppercase'>
+						Searchclan is one-stop solution for all travel voes
+					</div>
+				</div>
 				<img src={tbo} alt='TBO' className='w-40' />
 			</div>
 			<div className=''>
