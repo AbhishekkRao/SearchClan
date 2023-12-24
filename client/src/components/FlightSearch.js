97bfed1fb168ebe8199ef861b9a66d10ca9af131
Abhishek Rao <abhishek.komarraju@gmail.com>
2023-12-24 14:41:14 +0530

    updated the flight and stay forms

--- client/src/components/FlightSearch.js
+++ client/src/components/FlightSearch.js
@@ -17,6 +17,13 @@ const FlightSearch = () => {
         console.log('Flights form submitted successfully!');
     };
 
+    const handleSwap = () => {
+        // Swap values of fromLocation and toLocation
+        const temp = fromLocation;
+        setFromLocation(toLocation);
+        setToLocation(temp);
+    };
+
     return (
         <div>
             <div className='flex mt-8 justify-between items-center'>
@@ -24,20 +31,24 @@ const FlightSearch = () => {
                     <label className='text-gray-700'>From</label>
                     <input
                         type='text'
-                        className='w-full mt-2 p-2 border rounded'
+                        className='w-full h-14 mt-2 p-2 border rounded'
                         placeholder='Enter departure location'
                         value={fromLocation}
                         onChange={(e) => setFromLocation(e.target.value)}
                     />
                 </div>
-                <button className='flex items-center justify-center mt-8 border border-black rounded-full w-6 h-6'>
-                    <Icon icon="tdesign:swap" width={20} />
+                <button
+                    className='flex items-center justify-center mt-8 border border-black rounded-full w-6 h-6'
+                    onClick={handleSwap}
+                    tabIndex={-1}
+                >
+                    <Icon icon="tdesign:swap" width={24} />
                 </button>
                 <div className='w-1/2'>
                     <label className='text-gray-700'>Going to</label>
                     <input
                         type='text'
-                        className='w-full mt-2 p-2 border rounded'
+                        className='w-full h-14 mt-2 p-2 border rounded'
                         placeholder='Enter arrival location'
                         value={toLocation}
                         onChange={(e) => setToLocation(e.target.value)}
@@ -49,7 +60,7 @@ const FlightSearch = () => {
                     <label className='text-gray-700'>Departure Date</label>
                     <input
                         type='date'
-                        className='w-full mt-2 p-2 border rounded'
+                        className='w-full h-14 mt-2 p-2 border rounded'
                         min={new Date().toISOString().split('T')[0]}
                         value={departureDate}
                         onChange={(e) => setDepartureDate(e.target.value)}
@@ -59,75 +70,73 @@ const FlightSearch = () => {
                     <label className='text-gray-700'>Arrival Date</label>
                     <input
                         type='date'
-                        className='w-full mt-2 p-2 border rounded'
+                        className='w-full h-14 mt-2 p-2 border rounded'
                         min={new Date().toISOString().split('T')[0]}
                         value={arrivalDate}
                         onChange={(e) => setArrivalDate(e.target.value)}
                     />
                 </div>
             </div>
-            <div className='flex gap-x-8 mt-6'>
-                <div className='flex flex-col justify-between'>
-                    <label className='text-gray-700'>Travellers & Cabin Class</label>
-                    <div className='flex mt-2'>
-                        <select
-                            className='p-2 border rounded'
-                            value={flightAdults}
-                            onChange={(e) => setFlightAdults(parseInt(e.target.value))}
-                        >
-                            {[1, 2, 3, 4, 5].map((num) => (
-                                <option key={num} value={num}>
-                                    {num} Adults
-                                </option>
-                            ))}
-                        </select>
-                        <select
-                            className='ml-2 p-2 border rounded'
-                            value={flightChildren}
-                            onChange={(e) => setFlightChildren(parseInt(e.target.value))}
-                        >
-                            {[0, 1, 2, 3, 4, 5].map((num) => (
-                                <option key={num} value={num}>
-                                    {num} Children
-                                </option>
-                            ))}
-                        </select>
-                        <select
-                            className='ml-2 p-2 border rounded'
-                            value={flightInfants}
-                            onChange={(e) => setFlightInfants(parseInt(e.target.value))}
-                        >
-                            {[0, 1, 2].map((num) => (
-                                <option key={num} value={num}>
-                                    {num} Infants
-                                </option>
-                            ))}
-                        </select>
-                        <select
-                            className='ml-2 p-2 border rounded'
-                            value={cabinClass}
-                            onChange={(e) => setCabinClass(e.target.value)}
-                        >
-                            {['Economy', 'Business', 'First Class'].map((option) => (
-                                <option key={option} value={option}>
-                                    {option}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
+            <div className='flex flex-col justify-between mt-6'>
+                <label className='text-gray-700'>Travellers & Cabin Class</label>
+                <div className='flex mt-2 gap-x-2'>
+                    <select
+                        className='w-1/4 h-14 p-2 border rounded'
+                        value={flightAdults}
+                        onChange={(e) => setFlightAdults(parseInt(e.target.value))}
+                    >
+                        {[1, 2, 3, 4, 5].map((num) => (
+                            <option key={num} value={num}>
+                                {num} Adults
+                            </option>
+                        ))}
+                    </select>
+                    <select
+                        className='w-1/4 h-14 p-2 border rounded'
+                        value={flightChildren}
+                        onChange={(e) => setFlightChildren(parseInt(e.target.value))}
+                    >
+                        {[0, 1, 2, 3, 4, 5].map((num) => (
+                            <option key={num} value={num}>
+                                {num} Children
+                            </option>
+                        ))}
+                    </select>
+                    <select
+                        className='w-1/4 h-14 p-2 border rounded'
+                        value={flightInfants}
+                        onChange={(e) => setFlightInfants(parseInt(e.target.value))}
+                    >
+                        {[0, 1, 2].map((num) => (
+                            <option key={num} value={num}>
+                                {num} Infants
+                            </option>
+                        ))}
+                    </select>
+                    <select
+                        className='w-1/4 p-2 border rounded'
+                        value={cabinClass}
+                        onChange={(e) => setCabinClass(e.target.value)}
+                    >
+                        {['Economy', 'Business', 'First Class'].map((option) => (
+                            <option key={option} value={option}>
+                                {option}
+                            </option>
+                        ))}
+                    </select>
                 </div>
             </div>
             <div className='mt-6'>
                 <label className='text-gray-700'>Additional Inputs</label>
                 <input
                     type='text'
-                    className='w-full mt-2 p-2 border rounded'
+                    className='w-full h-14 mt-2 p-2 border rounded'
                     placeholder='Any additional preferences'
                     value={flightAdditionalInputs}
                     onChange={(e) => setFlightAdditionalInputs(e.target.value)}
                 />
             </div>
-            <div className='mt-6'>
+            <div className='mt-6 flex justify-end'>
                 <button
                     className='bg-blue-500 text-white px-6 py-2 rounded-full hover:bg-blue-600'
                     onClick={handleFlightsFormSubmit}
--- client/src/components/StaySearch.js
+++ client/src/components/StaySearch.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Icon } from '@iconify/react';
 
 const StaySearch = () => {
 
@@ -18,20 +19,29 @@ const StaySearch = () => {
         <div>
             <div className='mt-8'>
                 <label className='text-gray-700'>Destination</label>
-                <input
-                    type='text'
-                    className='w-full mt-2 p-2 border rounded'
-                    placeholder='Enter your destination'
-                    value={destination}
-                    onChange={(e) => setDestination(e.target.value)}
-                />
+                <span className='flex flex-col items-end mt-2'>
+                    <input
+                        type='text'
+                        className='w-full h-14 p-2 border rounded'
+                        placeholder='Enter your destination'
+                        value={destination}
+                        onChange={(e) => setDestination(e.target.value)}
+                    />
+                    <Icon
+                        icon="mdi:location"
+                        height={26}
+                        color='#1345AA'
+                        className='relative -top-10 -left-2 z-10'
+                        tabIndex={-1}
+                    />
+                </span>
             </div>
-            <div className='flex gap-x-8 mt-6'>
+            <div className='flex gap-x-8'>
                 <div className='w-1/2'>
                     <label className='text-gray-700'>Check In</label>
                     <input
                         type='date'
-                        className='w-full mt-2 p-2 border rounded'
+                        className='w-full h-14 mt-2 p-2 border rounded'
                         min={new Date().toISOString().split('T')[0]}
                         value={checkInDate}
                         onChange={(e) => setCheckInDate(e.target.value)}
@@ -41,64 +51,64 @@ const StaySearch = () => {
                     <label className='text-gray-700'>Check Out</label>
                     <input
                         type='date'
-                        className='w-full mt-2 p-2 border rounded'
+                        className='w-full h-14 mt-2 p-2 border rounded'
                         min={new Date().toISOString().split('T')[0]}
                         value={checkOutDate}
                         onChange={(e) => setCheckOutDate(e.target.value)}
                     />
                 </div>
             </div>
-            <div className='flex gap-x-8 mt-6'>
-                <div className=''>
+            <div className='mt-6'>
+                <div>
                     <label className='text-gray-700'>Rooms & Guests</label>
-                    <div className='flex mt-2'>
-                        <select
-                            className='p-2 border rounded'
-                            value={rooms}
-                            onChange={(e) => setRooms(parseInt(e.target.value))}
-                        >
-                            {[1, 2, 3, 4, 5].map((num) => (
-                                <option key={num} value={num}>
-                                    {num} Rooms
-                                </option>
-                            ))}
-                        </select>
-                        <select
-                            className='ml-2 p-2 border rounded'
-                            value={adults}
-                            onChange={(e) => setAdults(parseInt(e.target.value))}
-                        >
-                            {[1, 2, 3, 4, 5].map((num) => (
-                                <option key={num} value={num}>
-                                    {num} Adults
-                                </option>
-                            ))}
-                        </select>
-                        <select
-                            className='ml-2 p-2 border rounded'
-                            value={children}
-                            onChange={(e) => setChildren(parseInt(e.target.value))}
-                        >
-                            {[0, 1, 2, 3, 4, 5].map((num) => (
-                                <option key={num} value={num}>
-                                    {num} Children
-                                </option>
-                            ))}
-                        </select>
-                    </div>
+                </div>
+                <div className='flex mt-2 gap-x-2 h-14'>
+                    <select
+                        className='w-1/3 p-2 border rounded'
+                        value={rooms}
+                        onChange={(e) => setRooms(parseInt(e.target.value))}
+                    >
+                        {[1, 2, 3, 4, 5].map((num) => (
+                            <option key={num} value={num}>
+                                {num} Rooms
+                            </option>
+                        ))}
+                    </select>
+                    <select
+                        className='w-1/3 p-2 border rounded'
+                        value={adults}
+                        onChange={(e) => setAdults(parseInt(e.target.value))}
+                    >
+                        {[1, 2, 3, 4, 5].map((num) => (
+                            <option key={num} value={num}>
+                                {num} Adults
+                            </option>
+                        ))}
+                    </select>
+                    <select
+                        className='w-1/3 p-2 border rounded'
+                        value={children}
+                        onChange={(e) => setChildren(parseInt(e.target.value))}
+                    >
+                        {[0, 1, 2, 3, 4, 5].map((num) => (
+                            <option key={num} value={num}>
+                                {num} Children
+                            </option>
+                        ))}
+                    </select>
                 </div>
             </div>
             <div className='mt-6'>
                 <label className='text-gray-700'>Additional Inputs</label>
                 <input
                     type='text'
-                    className='w-full mt-2 p-2 border rounded'
+                    className='w-full mt-2 p-2 border rounded h-14'
                     placeholder='Any additional preferences'
                     value={additionalInputs}
                     onChange={(e) => setAdditionalInputs(e.target.value)}
                 />
             </div>
-            <div className='mt-6'>
+            <div className='mt-6 flex justify-end'>
                 <button
                     className='bg-blue-500 text-white px-6 py-2 rounded-full hover:bg-blue-600'
                     onClick={handleStayFormSubmit}
